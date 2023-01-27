120bf2ffd335f1117ede450c85633b358703c181
divo <divoapple@gmail.com>
2023-01-27 13:00:39 +0000

    Working lambda handler
    Tested with AWS console tools

--- lambda/app/handler.rb
+++ lambda/app/handler.rb
@@ -1,10 +1,57 @@
 require 'json'
+require 'securerandom'
+require "uri"
+require "net/http"
+
+
+SHEETS_URL = ENV["SHEETS_URL"]
 
 def handler(event:, context:)
-  data = JSON.parse(event['body'], object_class: OpenStruct)
+  data = JSON.parse(event['body'].to_json, object_class: OpenStruct)
 
-  return {
-    "statusCode": 200,
-    "body": data.event.test,
+
+  raise "Invalid data" if(!data || !data.events)
+
+  # TODO: Sort the events, probably need to parse timestamps
+  data.events.sort_by! { |e| e.timestamp }
+
+  # Value to save in sheets
+  session_data = {
+    sessionId: SecureRandom.uuid,
+    startedAt: data.events[0].timestamp,
   }
+
+  # Events
+  session_data[:events] = data.events.map do |event|
+    {
+      sessionId: session_data[:sessionId],
+      timestamp: event.timestamp,
+      event: event.eventName
+    }
+  end
+
+  # Post to Google Sheets
+  begin
+    url = URI(SHEETS_URL)
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Post.new(url)
+    request["Content-Type"] = "application/json"
+    request.body = session_data.to_json
+    response = https.request(request)
+
+    return {
+      "statusCode": 200,
+      "body": response.read_body
+    }
+  rescue => e
+    puts e.message
+    return {
+      statusCode: 500,
+      body: { msg: e.message }.to_json
+    }
+
+  end
 end
